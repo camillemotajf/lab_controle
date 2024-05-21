7be67bfcbc56c8f146729e1cbbcdff69598c0567
camillemotajf <camillemotajf@hotmail.com>
2024-05-21 09:14:29 -0300

    Modificações no codigo da resp ao degrau

--- src/velocidade.cpp
+++ src/velocidade.cpp
@@ -22,64 +22,27 @@
 // // Objeto Encoder
 // Encoder encoder(ENCA, ENCB);
 
-// // void calculateCurrentSpeed() {
-// //   // Calcula o tempo decorrido desde a última atualização
-// //   unsigned long currentTime = micros();
-// // //   Serial.print(currentTime);
-// // //   long elapsedTime = (currentTime - lastTime);  // Tempo decorrido em segundos
-// // //   lastTime = currentTime;
-
-// //   // Calcula a mudança na posição do encoder desde a última atualização
-// //   long currentPosition = encoder.read();
-// // //   long positionChange = (currentPosition - lastPos_i);
-// // //   lastPos_i = currentPosition;
-
-// //   // Calcula a velocidade em unidades de posição por segundo
-// //   speed = (currentPosition * 60)/(200 * currentTime * 1000000);
-
-// //   // Converte a velocidade para RPM
-// // //   float rpm = (speed / PPR) * 60.0;  // RPM = (velocidade / PPR) * 60
-
-// //   // Exibe a velocidade calculada em RPM no Serial Monitor
-// // //   Serial.print(typedef(speed));
-// //   Serial.print("PWM: ");
-// //   Serial.print(PWM_VALUE); // Exibe o valor de PWM utilizado
-// //   Serial.print(", Velocidade: ");
-// //   Serial.print(speed);
-// //   Serial.print(" Revolucoes/tempo ");
-// //   Serial.print(currentTime);
-// //   Serial.print(" vasco ");
-// //   Serial.print(currentPosition);
-// //   Serial.println("posicao atual");
-// // }
-
 // void calculateCurrentSpeed() {
-//    // Calcula o tempo decorrido desde a última atualização em segundos (delta t)
-//    unsigned long currentTime = micros();
-//    double elapsedTime = (currentTime - lastTime) / 1000000.0;  // Tempo decorrido em segundos
 
-//    // Calcula a mudança na posição do encoder desde a última atualização
-//    long currentPosition = encoder.read();
-//    long positionChange = currentPosition - lastPos_i;
-//    lastPos_i = currentPosition;
+//   // Calcula o tempo decorrido desde a última atualização
+//   unsigned long currentTime = micros();
 
-//    // Calcula a velocidade em rotações por segundo (RPS)
-//    double speedRPS = (double)positionChange / (PPR * elapsedTime);  // RPS = ΔPos / (PPR * Δt)
+//   // Calcula a mudança na posição do encoder desde a última atualização
+//   long currentPosition = encoder.read();
+//   long positionChange = (currentPosition - lastPos_i);
+//   lastPos_i = currentPosition;
 
-//    // Converte a velocidade para rotações por minuto (RPM)
-//    double speedRPM = speedRPS * 60.0;  // RPM = RPS * 60
+//   // Calcula a velocidade em unidades de posição por segundo
+//   speed = (currentPosition * 60)/(200 * currentTime * 1000000);
 
-//    // Atualiza o último tempo para o próximo cálculo
-//    lastTime = currentTime;
+//   // Converte a velocidade para RPM
+//   float rpm = (speed / PPR) * 60.0;  // RPM = (velocidade / PPR) * 60
 
-//    // Exibe a velocidade calculada no Serial Monitor
-//   //  Serial.print("Velocidade (RPM): ");
-//    Serial.print(speedRPM);
-//    Serial.print(",");
-//    Serial.println(currentTime);
-//   //  Serial.print(",");
+//   // Exibe a velocidade calculada em RPM no Serial Monitor
+//   Serial.print("Velocidade: ");
+//   Serial.println(speed);
+
 // }
-
 
 
 // void setup() {
@@ -100,13 +63,10 @@
 //   digitalWrite(IN2, LOW);
 
 //   // Define a velocidade do motor com base no valor arbitrário de PWM
-//   analogWrite(ENA, PWM_VALUE);  // Define o PWM para controlar a velocidade do motor
+//   analogWrite(ENA, PWM_VALUE);
 
 //   // Calcula e exibe a velocidade do motor em RPM
 //   calculateCurrentSpeed();
-
-//   // Aguarda um intervalo de tempo antes de calcular a velocidade novamente
-// //   delay(100);
 // }
 
-// // Função para calcular a velocidade atual do encoder em RPM
+// // // Função para calcular a velocidade atual do encoder em RPM
