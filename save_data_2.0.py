ca229b78baa441cfc86197057f255bcb6d095dc4
camillemotajf <camillemotajf@hotmail.com>
2024-05-21 17:31:18 -0300

    Plotando o gráfico de saída. Vel.Print = 0

--- save_data_2.0.py
+++ save_data_2.0.py
@@ -3,17 +3,17 @@ import csv
 import time
 
 # Configurações da porta serial
-serial_port = 'COM8'  # Defina a porta serial correta (exemplo para Linux)
-baud_rate = 9600
+serial_port = 'COM7'  # Defina a porta serial correta (exemplo para Linux)
+baud_rate = 115200
 
 # Abre a porta serial
 ser = serial.Serial(serial_port, baud_rate)
 
 # Abre o arquivo CSV para escrita
-csv_file_path = 'dados_senoide_100hz.csv'
+csv_file_path = 'dados_senoide_0.1hz.csv'
 csv_file = open(csv_file_path, 'w', newline='')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['tempo', 'tensao'])
+csv_writer.writerow(['tensao'])
 
 try:
     # Loop para ler continuamente os dados da porta serial
@@ -28,16 +28,16 @@ try:
         # print("Primeira linha ignorada:", line)
             continue
 
-        rpm_value, current_time = line.split(',')
+        rpm_value = line
         print("o trem la: ", rpm_value)
         rpm_value = float(rpm_value.split()[0])
-        current_time = float(current_time.split()[0]) # Extrai apenas o valor numérico da velocidade RPM
+        # current_time = float(current_time.split()[0]) # Extrai apenas o valor numérico da velocidade RPM
 
         # Obtém o tempo atual em milissegundos
         # current_time = int(time.time() * 1000)
     
         # Escreve os dados no arquivo CSV
-        csv_writer.writerow([current_time, rpm_value])
+        csv_writer.writerow([rpm_value])
 
 
 except KeyboardInterrupt:
--- src/senoide.cpp
+++ src/senoide.cpp
@@ -5,7 +5,7 @@
 #include <Encoder.h>
 
 #define AMPLITUDE 100
-#define FREQUENCIA 100 // Defina a frequência desejada aqui
+#define FREQUENCIA 0.1// Defina a frequência desejada aqui
 // #include <typeinfo>
 
 // Definições de pinos para o motor e encoder
@@ -28,41 +28,40 @@ double speed = 0.0;                   // Velocidade calculada do encoder em unid
 Encoder encoder(ENCA, ENCB);
 
 void calculateCurrentSpeed() {
-   // Calcula o tempo decorrido desde a última atualização em segundos (delta t)
-   unsigned long currentTime = micros();
-   double elapsedTime = (currentTime - lastTime) / 1000000.0;  // Tempo decorrido em segundos
 
-   // Calcula a mudança na posição do encoder desde a última atualização
-   long currentPosition = encoder.read();
-   long positionChange = currentPosition - lastPos_i;
-   lastPos_i = currentPosition;
+  // Calcula o tempo decorrido desde a última atualização
+  unsigned long currentTime = micros();
 
-   // Calcula a velocidade em rotações por segundo (RPS)
-   double speedRPS = (double)positionChange / (PPR * elapsedTime);  // RPS = ΔPos / (PPR * Δt)
+  // Calcula a mudança na posição do encoder desde a última atualização
+  long currentPosition = encoder.read();
+  long positionChange = (currentPosition - lastPos_i);
+  lastPos_i = currentPosition;
 
-   // Converte a velocidade para rotações por minuto (RPM)
-   double speedRPM = speedRPS * 60.0;  // RPM = RPS * 60
+  // Calcula a velocidade em unidades de posição por segundo
+  speed = (currentPosition * 60)/(200 * currentTime * 1000000);
 
-   // Atualiza o último tempo para o próximo cálculo
-   lastTime = currentTime;
-   float timeInSeconds = currentTime / 1000000.0;
+  // Converte a velocidade para RPM
+  float rpm = (speed / PPR) * 60.0;  // RPM = (velocidade / PPR) * 60
 
-   // Exibe a velocidade calculada no Serial Monitor
-  //  Serial.print("Velocidade (RPM): ");
-    Serial.println(speedRPM);
-//    Serial.print(",");
-//    Serial.println(timeInSeconds);
+  // Exibe a velocidade calculada em RPM no Serial Monitor
+  Serial.print("Velocidade: ");
+  Serial.println(speed);
+
 }
 
 int t = 0;
 
 void setup() {
+
   Serial.begin(115200); // Inicia a comunicação serial
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENCA, INPUT);
   pinMode(ENCB, INPUT);
+
+  lastTime = micros();
+  lastPos_i = encoder.read();
 
   digitalWrite(IN1,HIGH);
   digitalWrite(IN2,LOW);
@@ -71,14 +70,11 @@ void setup() {
 
 void loop() {
 //   for (int t = 0; t < 360; t++) { // Loop de 0 a 360 graus
-    
-  unsigned long currentTime = micros();
-  // float radianos = t * (PI / 180.0); // Converte graus para radianos
-  int valor = AMPLITUDE * sin(2*PI*FREQUENCIA * micros()/1000000.0) + AMPLITUDE; // Calcula o valor da onda senoidal
-  float elapsedTime = (currentTime)/ 1000000.0;
+    // float radianos = t * (PI / 180.0); // Converte graus para radianos
+  double valor = AMPLITUDE * sin(2*PI*FREQUENCIA * micros()/1000000.0) + AMPLITUDE; // Calcula o valor da onda senoidal
   analogWrite(ENA, valor);
-  calculateCurrentSpeed();
-  // Serial.println(valor);
+  // calculateCurrentSpeed();
+  Serial.println(valor);
   // Serial.print(",");
   // Serial.println(elapsedTime);
 
